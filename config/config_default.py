2e391c83702f42862d6c1e8974d6d976ddcbf2d1
Roberto Butti <roberto.butti@h-art.com>
2017-04-11 11:02:11 +0200

    First Commit

---
+++ config/config_default.py
@@ -0,0 +1,6 @@
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    APIAI_CLIENT_ACCESS_TOKEN=""
+    APIAI_USER_ACCESS_TOKEN=""
+    DATABASE_URI = 'sqlite://:memory:'
---
+++ web.py
@@ -0,0 +1,43 @@
+from flask import Flask,request, session, g, redirect, url_for, abort, render_template, flash
+from flask import jsonify
+import json
+from config.config_local import *
+
+import apiai
+
+app = Flask(__name__)
+
+
+app.config.from_object('config.config_local.DevelopmentConfig')
+app.config.from_object(__name__)
+
+app.config.from_envvar('FORTYTWO_SETTINGS', silent=True)
+
+
+
+
+
+@app.route('/')
+def index():
+    last_questions=1
+    return render_template('index.html',last_questions=last_questions)
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    text =request.form["text"]
+    ai = apiai.ApiAI(app.config.get("APIAI_CLIENT_ACCESS_TOKEN"))
+    ai_request = ai.text_request()
+    ai_request.lang = 'it'  # optional, default value equal 'en'
+    ai_request.session_id = app.config.get("APIAI_USER_ACCESS_TOKEN")
+    ai_request.query = text
+    response = ai_request.getresponse()
+    print(response.status)
+    r = json.loads(response.read())
+    return jsonify(answer=r['result']['fulfillment']['speech'], last_questions=None, answer_prob=1.0)
+
+    #return response.read()
+
+
+if __name__ == "__main__":
+	app.run()
