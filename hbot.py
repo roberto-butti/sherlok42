dd580a7e324c30fef7c40e1f4b046ee83a6def90
Roberto Butti <roberto.butti@h-art.com>
2017-04-13 11:39:27 +0200

    renaming hbot

--- hbot.py
+++ hbot.py
@@ -21,23 +21,33 @@ def index():
 @app.route('/send', methods=['POST'])
 def send():
     try:
+
         text =request.form["text"]
+        print(text)
         ai = apiai.ApiAI(app.config.get("APIAI_CLIENT_ACCESS_TOKEN"))
         ai_request = ai.text_request()
         ai_request.lang = 'it'  # optional, default value equal 'en'
         ai_request.session_id = app.config.get("APIAI_USER_ACCESS_TOKEN")
         ai_request.query = text
+        print("ready...")
         response = ai_request.getresponse()
+        print("response")
+        print(response)
         print(response.status)
         if (response.status == 200):
-            r = json.loads(response.read())
+            str = response.read().decode("utf8")
+            print(str)
+            r = json.loads(str)
+            print("read")
+            print(r)
             return jsonify(answer=r['result']['fulfillment']['speech'], last_questions=None, answer_prob=1.0)
         else:
             return jsonify(answer="I had some problems with my brain", last_questions=None, answer_prob=1.0)
     except:
+        raise
         return jsonify(answer="ERROR: I had some problems with my brain", last_questions=None, answer_prob=1.0)
 
 
-#if __name__ == "__main__":
-if __name__ == 'hbot':
+
+if __name__ == '__main__':
     app.run()
